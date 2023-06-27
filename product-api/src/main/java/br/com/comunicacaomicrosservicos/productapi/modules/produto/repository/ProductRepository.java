38a537a38b1edd19f73240b42fd75bc4045b470f
rafaelbraf <rafabrunofelix@hotmail.com>
2023-06-27 00:58:30 -0300

    Adiciona Repository de Produto

--- product-api/src/main/java/br/com/comunicacaomicrosservicos/productapi/modules/produto/repository/ProductRepository.java
+++ product-api/src/main/java/br/com/comunicacaomicrosservicos/productapi/modules/produto/repository/ProductRepository.java
@@ -1,2 +1,7 @@
-package br.com.comunicacaomicrosservicos.productapi.modules.produto.repository;public interface ProductRepository {
+package br.com.comunicacaomicrosservicos.productapi.modules.produto.repository;
+
+import br.com.comunicacaomicrosservicos.productapi.modules.produto.model.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProductRepository extends JpaRepository<Product, Integer> {
 }
