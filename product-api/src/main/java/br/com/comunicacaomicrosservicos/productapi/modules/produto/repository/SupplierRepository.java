e969297a937ff47a1c571c1ff5811f1f1b7ef81d
rafaelbraf <rafabrunofelix@hotmail.com>
2023-06-27 00:58:50 -0300

    Adiciona Repository de Fornecedor

--- product-api/src/main/java/br/com/comunicacaomicrosservicos/productapi/modules/produto/repository/SupplierRepository.java
+++ product-api/src/main/java/br/com/comunicacaomicrosservicos/productapi/modules/produto/repository/SupplierRepository.java
@@ -1,2 +1,7 @@
-package br.com.comunicacaomicrosservicos.productapi.modules.produto.repository;public interface SupplierRepository {
+package br.com.comunicacaomicrosservicos.productapi.modules.produto.repository;
+
+import br.com.comunicacaomicrosservicos.productapi.modules.produto.model.Supplier;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface SupplierRepository extends JpaRepository<Supplier, Integer> {
 }
